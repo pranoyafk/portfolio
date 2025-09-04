66c728a4d8277166f2859fefbc04c71e38e4624b
Pranoy Majumdar <works.pranoy@gmail.com>
2025-09-04 13:22:04 +0530

    feat: add some consts

---
+++ src/utils/constants.ts
@@ -0,0 +1,17 @@
+export interface INavlinksTypes {
+  label: string;
+  url: string;
+}
+
+export const navLinks: INavlinksTypes[] = [
+  {
+    label: "LinkedIn",
+    url: "https://linkedin.com/in/pranoyafk",
+  },
+  {
+    label: "Github",
+    url: "https://github.com/pranoyafk",
+  },
+];
+
+export const skills: string[] = ["TypeScript", "React.Js", "Vue.Js", "Node.Js"];
